07411b13eb7d1d97598390a9d1b2ca4d1246e4f8
Aditya-Bang <adityabang15@gmail.com>
2025-07-31 16:06:23 -0400

    getting cora data

--- cpp/tests/manual.py
+++ cpp/tests/manual.py
@@ -1,8 +1,10 @@
 import os
 import time
 import torch
+import graphcuda
 from torch_geometric.datasets import Planetoid
 
 # Load Cora dataset
 dataset = Planetoid(root=os.path.abspath(os.path.join(os.path.dirname(__file__), '../../data')), name='Cora')
 data = dataset[0]  # Cora contains a single graph
+
