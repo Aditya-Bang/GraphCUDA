c93db370ef2efe01436c5afc86874ceb9d9aafc7
Aditya Bang <adityabang15@gmail.com>
2025-08-16 20:01:35 -0400

    removed unecessary imports

--- cpp/src/graphcuda/cuda/gemm_cublas.cuh
+++ cpp/src/graphcuda/cuda/gemm_cublas.cuh
@@ -1,14 +1,6 @@
 #pragma once
 
 #include <torch/extension.h>
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <cstdint>
-#include <iostream>
-#include <algorithm>
-#include <cassert>
-#include <cstdio>
-#include <cstdlib>
 #include <cublas_v2.h>
 #include <ATen/cuda/CUDAContext.h>
 
--- cpp/src/graphcuda/cuda/spmm_cusparse.cuh
+++ cpp/src/graphcuda/cuda/spmm_cusparse.cuh
@@ -1,17 +1,8 @@
 #pragma once
 
 #include <torch/extension.h>
-#include <torch/torch.h>
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <cstdint>
-#include <iostream>
-#include <algorithm>
-#include <cassert>
-#include <cstdio>
-#include <cstdlib>
-#include <cublas_v2.h>
 #include <ATen/cuda/CUDAContext.h>
+#include <cuda_runtime.h>
 #include <cusparse.h>
 
 
