c84477e0866bf347f1862306f5ecbb6759c0d30f
Aditya Bang <adityabang15@gmail.com>
2026-04-09 18:34:11 +0000

    update comment to have example point to tests dir

--- examples/gcn.py
+++ examples/gcn.py
@@ -1,1 +1,1 @@
-# TODO: Implement a GCN model using the GraphCUDA library, have equivalent PyG impl commented
+# TODO: Implement a GCN model using the GraphCUDA library (if currently looking for examples check tests/gcn/graphcuda_impl.py), have equivalent PyG impl commented
